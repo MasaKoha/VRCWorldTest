88095493c3809a792048249a6d838981a6007c24
まさこは <masalinkinpark@gmail.com>
2024-10-27 00:28:27 +0900

    WIP

---
+++ Assets/_Test/Experimental/RotateCube.cs
@@ -0,0 +1,31 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace _Test.Experimental
+{
+    public sealed class RotateCube : UdonSharpBehaviour
+    {
+        private float _angle = 90f;
+        private bool _isOn = true;
+
+        public void Initialize(float angle)
+        {
+            _angle = angle;
+        }
+
+        private void Update()
+        {
+            if (_isOn)
+            {
+                transform.Rotate(Vector3.up, _angle * Time.deltaTime);
+            }
+        }
+
+        public void Toggle()
+        {
+            _isOn = !_isOn;
+        }
+    }
+}
